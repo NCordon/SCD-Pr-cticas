6d75e189a7cefd09f2c0b279613d33be6985b376
NCordon <nacho.cordon.castillo@gmail.com>
2015-04-12 20:09:07 +0200

    Changes the way each thread evaluates values

--- pi.cc
+++ pi.cc
@@ -6,13 +6,21 @@ using namespace std;
 // Number of processors
 const unsigned int n=4;
 // Number of threads. Must be a multiple of n
-const unsigned int m=2500;
+const unsigned int m=9000;
 double sums[m];
 
 // Function f(x) = 4/(1+x^2)
 void* f(void* x){
-    double arg = 1.0 * (unsigned long) x/ m;
-    sums[(unsigned long)x] = 4.0/(1+arg*arg);
+    unsigned long j = (unsigned long) x;
+    double arg;
+    
+    for(unsigned int i=0; i<m/n; ++i){
+        arg = (i + j*m/n);
+        arg = arg/m;
+        sums[j] += 4.0/(1+arg*arg);
+    }
+    
+    return NULL;
 }
 
 
@@ -22,12 +30,9 @@ int main(){
     void * arg;
     unsigned long x;
 
-    for(unsigned int i=0; i<m/n; ++i){
-	for(unsigned int j=0; j<n; ++j){
-	    x = (i*n + j);
-	    arg = (void*) x;
-	    pthread_create (&(hebras[j]), NULL, f, arg);
-	}
+    for(unsigned long i=0; i<n; ++i){
+        arg = (void*) i;
+        pthread_create (&(hebras[i]), NULL, f, arg);
     }
 
     for(unsigned int j=0; j<n; ++j){
